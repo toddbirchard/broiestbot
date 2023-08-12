9826c9621179fc971444d12e3e37f8e17cbd24b4
Todd Birchard <toddbirchard@gmail.com>
2023-08-12 17:06:08 -0400

    Update ORM session to `autoflush` and `autobegin`.

--- database/__init__.py
+++ database/__init__.py
@@ -7,5 +7,5 @@ from config import DATABASE_ARGS, SQLALCHEMY_DATABASE_URI
 
 # Initialize Read/write database Session
 engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args=DATABASE_ARGS, echo=False)
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine, autoflush=True, autobegin=True)
 session = Session()
