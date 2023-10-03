2edec25b2aefe8d204df1ca31243a1258061708c
Todd Birchard <toddbirchard@gmail.com>
2023-10-03 14:48:05 -0400

    Rough inital draft of footy stats.

--- broiestbot/commands/footy/stats.py
+++ broiestbot/commands/footy/stats.py
@@ -1,16 +1,14 @@
 """Get general fixture stats for live fixtures."""
 from typing import Optional
-from collections import ChainMap
-
-
 import requests
-
-from .util import get_preferred_timezone
-from logger import LOGGER
-from .live import fetch_live_fixtures
-from config import FOOTY_FIXTURE_STATS_ENDPOINT, FOOTY_HTTP_HEADERS, FOOTY_LIVE_STATS_LEAGUES, HTTP_REQUEST_TIMEOUT
 from emoji import emojize
 from chatango.ch import Room
+
+from logger import LOGGER
+from config import FOOTY_FIXTURE_STATS_ENDPOINT, FOOTY_HTTP_HEADERS, FOOTY_LIVE_STATS_LEAGUES, HTTP_REQUEST_TIMEOUT
+
+from .live import fetch_live_fixtures
+from .util import get_preferred_timezone
 
 
 def footy_stats_for_live_fixtures(room: Room, username: str):
@@ -27,7 +25,7 @@ def footy_stats_for_live_fixtures(room: Room, username: str):
         for league_name, league_id in FOOTY_LIVE_STATS_LEAGUES.items():
             live_league_fixtures = fetch_live_fixtures(league_id, tz_name)
             if live_league_fixtures and bool(live_league_fixtures) and live_league_fixtures != []:
-                live_fixture_stats_response += f"<b>{league_name}<b>\n"
+                live_fixture_stats_response += f"<b>{league_name}</b>\n"
                 for fixture in live_league_fixtures:
                     if fixture.get("fixture"):
                         fixture_id = fixture["fixture"]["id"]
@@ -57,7 +55,7 @@ def fetch_stats_per_live_fixture(fixture_id: int) -> Optional[str]:
             params=params,
             timeout=HTTP_REQUEST_TIMEOUT,
         )
-        return resp.json()
+        return resp.json()["response"]
     except requests.HTTPError as e:
         LOGGER.error(f"HTTPError while fetching live fixtures: {e.response.content}")
     except KeyError as e:
@@ -73,37 +71,38 @@ def parse_live_fixture_stats(fixture_stats: dict) -> str:
     return: str
     """
     try:
-        fixture_stats_response = "\n\n"
+        fixture_stats_response = "\n"
         LOGGER.info(f"fixture_stats: {fixture_stats}")
-        team_name = fixture_stats["response"]["team"]["name"]
-        fixture_stats_response += f"<b>{team_name}</b>\n"
-        for team_stats in fixture_stats["response"]:
-            stat_dict = ChainMap(team_stats)
-            LOGGER.info(f"stat_dict: {stat_dict}")
-            """for stat in team_fixture_stats:
-                    # if stat["type"] == "type":
-                    LOGGER.info(f"stat: {stat}")
-                    team_statistics = team_fixture_stats["statistics"]
-                    possession = team_statistics.get("Ball Possession", 0)
-                    sog = team_statistics.get("Shots on Goal", 0)
-                    total_shots = team_statistics.get("Total Shots", 0)
-                    fouls = team_statistics.get("Fouls", 0)
-                    yellow_cards = team_statistics.get("Yellow Cards", 0)
-                    red_cards = team_statistics.get("Red Cards", 0)
-                    pass_accuracy = team_statistics.get("Passes %", 0)
-                    xg = team_statistics.get("Expected Goals", 0.0)
-                    fixture_stats_response += f"<b>{team_name}</b>\n \
-                                            :soccer_ball: POSSESSION: {possession}\n \
-                                            :bullseye: SOG: {sog} ({total_shots} TOTAL SHOTS) \n \
-                                            :counterclockwise_arrows_button: PASS ACCURACY: {pass_accuracy}\n \
-                                            :no_entry: FOULS: {fouls} (:yellow_square: {yellow_cards}, :red_square: {red_cards})\n \
+        for i, team_fixture_stats in enumerate(fixture_stats):
+            team_stats = unpack_team_statistics(team_fixture_stats["statistics"])
+            LOGGER.info(f"team_stats: {team_stats}")
+            team_name = team_fixture_stats["team"]["name"]
+            possession = team_stats.get("Ball Possession", 0)
+            sog = team_stats.get("Shots on Goal", 0)
+            total_shots = team_stats.get("Total Shots", 0)
+            fouls = team_stats.get("Fouls", 0)
+            yellow_cards = team_stats.get("Yellow Cards", 0)
+            red_cards = team_stats.get("Red Cards", 0)
+            pass_accuracy = team_stats.get("Passes %", 0)
+            xg = team_stats.get("expected_goals")
+            fixture_stats_response += f"<b>{team_name}</b>\n \
+                                            :soccer_ball: Possession: {possession}\n \
+                                            :bullseye: SOG: {sog} (of {total_shots} total shots) \n \
+                                            :counterclockwise_arrows_button: Pass accuracy: {pass_accuracy}\n \
+                                            :no_entry: Fouls: {fouls} (:yellow_square: {yellow_cards}, :red_square: {red_cards})\n \
                                             :soccer_ball: xG: {xg} \n"
-                    if i == 0 and team_fixture_stats:
-                        fixture_stats_response += "\n-------------------\n"
-                    if i == len(fixture_stats - 1) and team_fixture_stats:
-                        return fixture_stats_response"""
-        return fixture_stats_response
+            if i == 0 and team_fixture_stats:
+                fixture_stats_response += "\n"
+        return f"{fixture_stats_response}\n-------------------\n\n"
     except ValueError as e:
         LOGGER.error(f"ValueError when parsing live fixture stats: {e}")
     except Exception as e:
         LOGGER.error(f"Unexpected error when parsing live fixture stats: {e}")
+
+
+def unpack_team_statistics(team_stats_list: list) -> dict:
+    """Normalize team stat data."""
+    team_stats = {}
+    for stat in team_stats_list:
+        team_stats[stat["type"]] = stat["value"] if stat["value"] else 0
+    return team_stats
