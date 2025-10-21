a4cf6af673c98fa464bb69a08da84023017beea2
Todd Birchard <toddbirchard@gmail.com>
2025-10-21 07:46:00 -0400

    Still broken Sleeper command.

--- broiestbot/commands/nfl/sleeper.py
+++ broiestbot/commands/nfl/sleeper.py
@@ -10,7 +10,7 @@ from logger import LOGGER
 def get_sleeper_week_number() -> Optional[int]:
     """
     Get the current NFL week number from Sleeper API.
-    
+
     :returns: Optional[int]
     """
     LOGGER.info("Fetching current NFL week number from Sleeper...")
@@ -35,19 +35,20 @@ def get_sleeper_league_users() -> dict:
         if response.status_code == 200:
             for league_user in response.json():
                 user = {
-                        league_user["user_id"]: {
-                            "user_id": league_user["user_id"],
-                            "username": league_user["display_name"],
-                            "avatar": league_user["metadata"].get("avatar"),
-                            "team_name": league_user["metadata"].get("team_name"),
-                            "commish": league_user.get("is_owner", False),
-                        }
+                    league_user["user_id"]: {
+                        "user_id": league_user["user_id"],
+                        "username": league_user["display_name"],
+                        "avatar": league_user["metadata"].get("avatar"),
+                        "team_name": league_user["metadata"].get("team_name"),
+                        "commish": league_user.get("is_owner", False),
                     }
+                }
                 league_users.update(user)
         return league_users
     except Exception as e:
         LOGGER.error(f"Error fetching Sleeper league users: {e}")
         return {}
+
 
 def fetch_sleeper_rosters(users: dict):
     """Fetch and return Sleeper fantasy football rosters."""
@@ -60,7 +61,6 @@ def fetch_sleeper_rosters(users: dict):
             for roster in response.json():
                 users[roster["owner_id"]].update({"roster_id": roster["roster_id"]})
                 roster_users.append(users[roster["owner_id"]])
-        LOGGER.info(f"USERS = {roster_users}")
         return roster_users
     except Exception as e:
         LOGGER.error(f"Error fetching Sleeper rosters: {e}")
@@ -74,7 +74,7 @@ def fetch_sleeper_matchups(username: str) -> str:
         week_number = get_sleeper_week_number()
         if week_number is None:
             return f"Sorry @{username}, NFL season is OVAH!!! MORAN!!!!"
-        
+
         users = get_sleeper_league_users()
         users = fetch_sleeper_rosters(users)
 
@@ -84,15 +84,20 @@ def fetch_sleeper_matchups(username: str) -> str:
         matchups_response = "\n\n\n"
 
         if response.status_code == 200:
-            matchups = sorted(response.json(), key=lambda m: m['matchup_id'])
-            for i in range(len(matchups) // 2):
-                team1 = matchups[i]
-                team2 = matchups[i + 1]
+            matchups = sorted(response.json(), key=lambda m: m["matchup_id"])
+            j = 0
+            for i in range(0, len(matchups), 2):
+                LOGGER.info("------------------------------")
+                team1 = matchups[j]
+                team2 = matchups[j + 1]
+                LOGGER.info(f"Matchup #{j} | i = {i}")
                 LOGGER.info(f"Team 1: {team1}")
                 LOGGER.info(f"Player 1: {users[team1['roster_id']]}")
                 LOGGER.info(f"Team 2: {team2}")
                 LOGGER.info(f"Player 2: {users[team2['roster_id']]}")
+                LOGGER.info("------------------------------")
                 matchups_response += f"<b>{users[team1['roster_id']]['team_name']}</b> ({users[team1['roster_id']]['username']}) vs <b>{users[team2['roster_id']]['team_name']}</b> ({users[team2['roster_id']]['username']})\n WIP lol \n\n"
+                j += 1
             LOGGER.info(f"MATCHUPS: {matchups_response}")
             return matchups_response
         else:
