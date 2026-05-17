ce1d417299750d93513d67ee94172064bbd88bd3
Todd Birchard <toddbirchard@gmail.com>
2026-05-17 09:17:05 -0400

    Update EAFC 26 ID.

--- clients/psn.py
+++ clients/psn.py
@@ -6,10 +6,9 @@ from psnawp_api import PSNAWP
 from psnawp_api.models.client import Client
 from psnawp_api.models.user import User
 
-from config import PLAYSTATION_EAFC_2025_ID
+from config import PLAYSTATION_EAFC_2026_ID
 
 # from psnawp_api.models.trophies.trophy import Trophy
-
 
 
 class PlaystationClient:
@@ -17,7 +16,7 @@ class PlaystationClient:
 
     def __init__(self, token: str):
         self.psn = PSNAWP(token)
-        self.eafc = PLAYSTATION_EAFC_2025_ID
+        self.eafc = PLAYSTATION_EAFC_2026_ID
 
     @property
     def account(self) -> Client:
@@ -28,11 +27,11 @@ class PlaystationClient:
         """
         return self.psn.me()
 
-    def get_online_friends(self) -> List[Optional[User]]:
+    def get_online_friends(self) -> List[User]:
         """
         Get friends of logged-in PSN user.
 
-        :returns: List[Optional[User]]
+        :returns: List[User]
         """
         friends = self.account.friends_list()
         online_friends = [
